cb84944dc96f401af687d60d69348b9d925072f4
yangil <yangil.lee@worksmobile.com>
2016-04-24 17:30:13 +0900

    네이버 지도 추가

--- static/js/common/naver.map.js
+++ static/js/common/naver.map.js
@@ -3,9 +3,9 @@
     context.naver.map = {
         _instance: {},
 
-        Instance: function(elId, locationX, locationY) {
+        create: function(elementId, locationX, locationY) {
             var targetPoint = new nhn.api.map.LatLng(locationX, locationY);
-            var targetEl = $('#' + elId);
+            var targetEl = $('#' + elementId);
             var width = targetEl.outerWidth() == 0 ? 500 : targetEl.outerWidth();
             var height = targetEl.outerHeight() == 0 ? width : targetEl.outerHeight();
             var oMap = new nhn.api.map.Map(targetEl.get(0), {
@@ -21,7 +21,7 @@
                 size : new nhn.api.map.Size(width, height)
             });
 
-            wcard.naver.map._instance[elId] = oMap;
+            wcard.naver.map._instance[elementId] = oMap;
 
             return oMap;
         },
