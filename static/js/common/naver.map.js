18f729ffe3dd015b6f918379ba3086483f3c4b4f
yangil <yangil.lee@worksmobile.com>
2016-04-25 14:52:37 +0900

    네이버 지도 추가

--- static/js/common/naver.map.js
+++ static/js/common/naver.map.js
@@ -45,8 +45,8 @@
             oMap.addOverlay(oMarker);
 
             var oLabel = new nhn.api.map.MarkerLabel(); // - 마커 라벨 선언.
-            oLabel.setVisible(oMarker, true);
             oMap.addOverlay(oLabel); // - 마커 라벨 지도에 추가. 기본은 라벨이 보이지 않는 상태로 추가됨.
+            oLabel.setVisible(oMarker, true);
         }
     };
 })(window.wcard = window.wcard || {})
